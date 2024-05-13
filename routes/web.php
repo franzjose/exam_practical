61fc83607bfc1c92902c05c848077d8deebe31d0
Francis <francisdecastrojse@gmail.com>
2024-05-13 15:05:41 +0800

    revert inertia renderig to welcom page

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Login', [
+    return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
